26812c9df92909e6c48cec72d76eff75d7460ba7
Webbpiraten <webbpiraten@gmail.com>
2014-05-22 16:30:56 +0200

    Update Prolog_lab4_othello.pl

--- Prolog_lab4_othello.pl
+++ Prolog_lab4_othello.pl
@@ -4,13 +4,34 @@
 % Black (b), White (w).
 
 
+% legalMove: list med alla som kan flippas.
 legalMove(Color, Board, X, Y).
+	xAxis(Color, Board, X),
+	yAxis(Color, Board, Y),
+	diagonal(Color, Board, X, Y).
+	
+	
+xAxis(Color, Board, X):-
 	
 
-makeMove(Color, Board, X, Y, NewBoard).
+yAxis(Color, Board, Y).
+	
+
+diagonal(Color, Board, X, Y).
+	
+
+% tar listan från legalMove och flippar, maplist funktion (samma som haskell).
+% ifall legalMove failar, låt andra spelaren göra sitt move.
+makeMove(Color, Board, X, Y, NewBoard):-
+	legalMove(Color, Board, X, Y).
 
 % -Moves, -NewBoard
-makeMoves(Color, Board, N, Moves, NewBoard).
+% kör makeMove N antal gånger.
+makeMoves(Color, Board, N, Moves, NewBoard):-
+	N>0,
+	makeMove(Color, Board, X, Y, NewBoard2),
+	N1 is N-1,
+	makeMoves(Color, NewBoard2, N1, Moves, NewBoard).
 
 % -Value
 valueOf(Color, Board, Value).
